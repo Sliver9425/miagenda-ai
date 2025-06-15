c7e69d67a17ef18c5602a31b97cfdd4023e341a5
Sliver9425 <damianalejandro.9422@gmail.com>
2025-06-15 01:50:36 -0500

    clerk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router-dom';
 
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
